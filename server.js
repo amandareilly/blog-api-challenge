b8a53fec3ffbc45d9357ec0d825a5f6d3813477e
Amanda Reilly <amandamichellereilly@gmail.com>
2018-04-27 21:47:51 -0700

    Update server.js to work with test suite.

--- server.js
+++ server.js
@@ -9,6 +9,41 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log('Your app is listening on port ${process.env.PORT || 8080}');
-});
+// server object for runServer and closeServer functions
+let server;
+
+// starts server and returns a promise.
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err)
+        });
+    });
+}
+
+// closes server and returns a promise
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                // so we don't also call `resolve()`
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+// handle case where server.js is called directly
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+// export commands
+module.exports = { app, runServer, closeServer };
